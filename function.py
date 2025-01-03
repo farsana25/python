d2475362459f521dff34d1e32dbe7963e04fa58d
farsana25 <noushadfarsana537@gmail.com>
2025-01-03 17:01:30 +0530

    work

---
+++ function.py
@@ -0,0 +1,58 @@
+# def fun(a,b):
+#     c=a+b
+#     print(c)
+# fun(5,3)
+# def fun(a,b):
+#     c=a*b
+#     print(c)
+# fun(2,3)
+
+
+
+# def fun(a,b):
+#     print(a+b,a-b,a*b,a/b)
+# fun(5,5)
+
+# for i in range (1,31):
+#     if i%3==0 and i%5==0:
+#         print('Farsana U S')
+#     elif i%3==0:
+#         print('farsana')
+#     elif i%5==0:
+#         print('U S')
+#     else:
+#         print(i)
+
+
+
+# def fun(a,b):
+#     for i in range(a,b):
+#         if i%3==0 and i%5==0:
+#             print('farsana us')
+#         elif i%3==0:
+#             print('farsana')
+#         elif i%5==0:
+#             print('us')
+#         else:
+#             print(i)
+# fun(1,31)    
+
+
+# n=[4,3,2,7,8,2,3,1]
+# s=set(n)
+# missing=[]
+# for i in range(1,len(n)+1):
+#     if i not in s:
+#         missing.append(i)
+# print(missing)    
+
+
+# a=int(input('enter the number'))
+# b=0
+# num=a
+# while num>0:
+#     d=num%10
+#     b=b+d
+#     num=num//10
+# print(b)
+
