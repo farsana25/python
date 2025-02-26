6d81afa2a4787c1a8fe11c39c9cbebfbccd6a835
farsana25 <noushadfarsana537@gmail.com>
2025-02-26 13:28:42 +0530

    new one.py

---
+++ new one.py
@@ -0,0 +1,24 @@
+# print('hello word')
+
+# for i in range(5):
+#     for j in range(i+1):
+#      print('a',end=' ')
+#     print()
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print('* ',end='')
+#     print()  
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print('$ ',end='')
+#     print()
+
+
+# i=1
+# j=0
+# for i in range(5):
+#     for j in range(5):
+#         print('# ',end='')
+#     print()
